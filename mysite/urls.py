0d07f846dfc56c8d95fea68b5b273e986aebd15b
Athénaïs Vaginay <vaginay.athenais@gmail.com>
2017-11-18 16:23:56 +0100

    Modification du HTML du site

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
+
+# liste de regex qui determine les pages à sevir en fonction de l'url
+# En Python, les expressions régulière commencent toujours par r au début de la chaîne de caractères. 
+# Cela permet d'indiquer à Python que ce qui va suivre inclut des caractères qu'il ne doit pas interpréter 
+# en tant que code Python mais en tant qu'expression régulière.
+
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls), # "celles qui commencent pas admin/"
+    url(r'', include('blog.urls')),
 ]
